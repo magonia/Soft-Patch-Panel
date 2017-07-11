4499cc3e88b0e906074a6978fc1b4bf9e58b7978
Takuya Kurakado <kurakado.takuya@po.ntt-tx.co.jp>
2017-07-11 12:33:49 +0900

    modify

--- test_script.sh
+++ test_script.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
-cmd="export RTE_SDK=/home/gitlab-runner/dpdk-17.05"
-echo "> $cmd"
-$cmd
+execute(){
+echo "> $1"
+$1
+return $?
+}
 
-cmd="export RTE_TARGET=x86_64-native-linuxapp-gcc"
-echo "> $cmd"
-$cmd
-
-cmd="source /home/gitlab-runner/.bashrc"
-echo "> $cmd"
-$cmd
-
-cmd="$RTE_SDK"
-echo "> $cmd"
-$cmd
-
-cmd="pwd"
-echo "> $cmd"
-$cmd
-
-cmd="cd src"
-echo "> $cmd"
-$cmd
-
-cmd="make"
-echo "> $cmd"
-$cmd
+execute "export RTE_SDK=/home/gitlab-runner/dpdk-17.05"
+execute "export RTE_TARGET=x86_64-native-linuxapp-gcc"
+execute "source /home/gitlab-runner/.bashrc"
+execute "pwd"
+execute "cd src"
+execute "make"
 exit $?
